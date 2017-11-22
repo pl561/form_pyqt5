bdfa8b197d33bbb8b02f1e0a23f1d316c3c44bae
plefevre <plefevre561@gmail.com>
2017-11-22 19:15:23 +0100

    updated timer code

---
+++ pomodoro.py
@@ -0,0 +1,117 @@
+#! /usr/bin/env python
+# -*-coding:utf-8 -*
+
+import sys
+import os
+
+from PyQt5 import QtWidgets, Qt, QtGui, QtCore
+from PyQt5.QtCore import pyqtSlot, pyqtSignal
+
+HOME = os.environ['HOME']
+
+# class TimerSignals(Qt.QObject):
+#     start = pyqtSignal()
+#     stop = pyqtSignal()
+#     toggle = pyqtSignal()
+
+class SelectiveWidget(QtWidgets.QWidget):
+    def __init__(self, mode="lcd", parent=None):
+        super().__init__(parent=parent)
+        self.mode = mode
+        if self.mode == "lcd":
+            self.widget = QtWidgets.QLCDNumber(parent=parent)
+        elif self.mode == "lbl":
+            self.widget = QtWidgets.QLabel(parent=parent)
+
+class MyTimer(QtCore.QTimer):
+    """customized timer
+       amount in seconds
+       by default timer is deactivated"""
+    start_signal = pyqtSignal(int)
+    tick_signal = pyqtSignal(int)
+    stop_signal = pyqtSignal()
+    def __init__(self, amount, parent=None):
+        super().__init__(parent=parent)
+        self.setInterval(1000)
+        self.set_amount(amount)
+        self.timeout.connect(self.update)
+        self.is_ticking = False
+
+    def get_amount(self):
+        return self.amount
+
+    def set_amount(self, amount):
+        self.amount = amount
+
+    def toggle(self):
+        if self.is_ticking:
+            self.stop_ticking()
+        else:
+            self.start_ticking()
+
+    def update(self):
+        if self.amount > 0:
+            self.amount += -1
+            self.tick_signal.emit(self.get_amount())
+        else:
+            self.stop_ticking()
+
+    def start_ticking(self):
+        self.start(1000)
+        self.start_signal.emit(self.get_amount())
+        self.is_ticking = True
+
+    def stop_ticking(self):
+        self.stop()
+        self.stop_signal.emit()
+        self.is_ticking = False
+
+
+class Pomodoro(QtWidgets.QMainWindow):
+    """main window containing the timer widget"""
+    def __init__(self, duration, parent=None):
+        super().__init__(parent=parent)
+        self.title = "Timer"
+        self.left, self.top = 10, 10
+        self.width, self.height = 300, 300
+        self.duration = duration
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        central_widget = QtWidgets.QWidget(parent=self)
+        self.setCentralWidget(central_widget)
+
+        dim = QtCore.QRect(0, 0, 200, 100)
+        self.display_widget = QtWidgets.QLCDNumber(parent=central_widget)
+        self.display_widget.setGeometry(dim)
+        self.display_widget.display(self.duration)
+
+        self.timer = MyTimer(self.duration)
+        self.timer.tick_signal.connect(self.display_widget.display)
+        self.timer.start_ticking()
+
+        # self.label = QtWidgets.QLabel(str(self.timer.get_amount()),
+        #                               parent=central_widget)
+
+
+
+
+
+    def print_countdown(self, amount):
+        self.label.setText(str(amount))
+        print(amount)
+
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    ex = Pomodoro(5)
+    ex.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    sys.exit(main())
