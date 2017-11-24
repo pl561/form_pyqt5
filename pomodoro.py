a9b55f414b573d2e03f87805165d33c46ad3dfed
plefevre <plefevre561@gmail.com>
2017-11-24 22:39:41 +0100

    updated pomodoro with qsound

--- pomodoro.py
+++ pomodoro.py
@@ -5,37 +5,25 @@ import sys
 import os
 
 from PyQt5 import QtWidgets, Qt, QtGui, QtCore
-from PyQt5.QtCore import pyqtSlot, pyqtSignal
+from PyQt5.QtMultimedia import QSound
+from PyQt5.QtCore import pyqtSignal
 
 HOME = os.environ['HOME']
-
-# class TimerSignals(Qt.QObject):
-#     start = pyqtSignal()
-#     stop = pyqtSignal()
-#     toggle = pyqtSignal()
-
-# class SelectiveWidget(QtWidgets.QWidget):
-#     def __init__(self, mode="lcd", parent=None):
-#         super().__init__(parent=parent)
-#         self.mode = mode
-#         if self.mode == "lcd":
-#             self.widget = QtWidgets.QLCDNumber(parent=parent)
-#         elif self.mode == "lbl":
-#             self.widget = QtWidgets.QLabel(parent=parent)
 
 class MyTimer(QtCore.QTimer):
     """customized timer
        amount in seconds
        by default timer is deactivated"""
-    start_signal = pyqtSignal(int)
+    start_signal = pyqtSignal(str)
     tick_signal = pyqtSignal(int)
-    stop_signal = pyqtSignal()
-    def __init__(self, amount, parent=None):
+    stop_signal = pyqtSignal(str)
+    def __init__(self, amount, sound_fname="", parent=None):
         super().__init__(parent=parent)
         self.setInterval(1000)
         self.set_amount(amount)
         self.timeout.connect(self.update)
         self.is_ticking = False
+        self.sound_obj = QSound(sound_fname, parent=self)
 
     def get_amount(self):
         return self.amount
@@ -55,15 +43,16 @@ class MyTimer(QtCore.QTimer):
             self.tick_signal.emit(self.get_amount())
         else:
             self.stop_ticking()
+            self.sound_obj.play()
 
     def start_ticking(self):
         self.start(1000)
-        self.start_signal.emit(self.get_amount())
+        self.start_signal.emit("started")
         self.is_ticking = True
 
     def stop_ticking(self):
         self.stop()
-        self.stop_signal.emit()
+        self.stop_signal.emit("stopped")
         self.is_ticking = False
 
 
@@ -88,18 +77,19 @@ class MyLCDNumber(QtWidgets.QLCDNumber):
 class Pomodoro(QtWidgets.QMainWindow):
     """main window containing the timer widget
        duration in minutes"""
-    def __init__(self, duration, parent=None):
+    def __init__(self, duration, sound_fname="", parent=None):
         super().__init__(parent=parent)
         self.title = "Pomodoro Timer"
         self.left, self.top = 10, 10
         self.width, self.height = 200, 150
         self.duration = duration
+        self.sound_fname = sound_fname
         self.init_ui()
 
     def init_ui(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        self.statusBar().showMessage("Stopped")
+        self.statusBar().showMessage("Click on LCD to start the timer.")
         central_widget = QtWidgets.QWidget(parent=self)
         self.setCentralWidget(central_widget)
 
@@ -108,35 +98,29 @@ class Pomodoro(QtWidgets.QMainWindow):
         self.display_widget = MyLCDNumber(parent=central_widget)
         self.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal,
                                         parent=central_widget)
-        self.slider.setMinimum(1)
+        self.slider.setMinimum(0)
         self.slider.setMaximum(60)
         self.slider.setValue(self.duration)
         self.slider.setTickInterval(5)
         self.slider.setTickPosition(QtWidgets.QSlider.TicksBothSides)
 
-        self.timer = MyTimer(self.duration)
+        self.timer = MyTimer(self.duration, self.sound_fname)
         self.timer.tick_signal.connect(self.display_widget.display_time)
-        self.timer.stop_signal.connect(self.show_stop_message)
+        self.timer.start_signal.connect(self.show_message)
+        self.timer.stop_signal.connect(self.show_message)
 
         self.update_slider_change(self.duration)
         self.slider.valueChanged.connect(self.update_slider_change)
-        self.display_widget.click_signal.connect(self.update_timer_status)
+
+        # when the LCD widget captures a click, toggle timer and status
+        self.display_widget.click_signal.connect(self.timer.toggle)
 
         layout.addWidget(self.display_widget)
         layout.addWidget(self.slider)
 
-
     def update_slider_change(self, amount):
         self.timer.set_amount(amount*60)
         self.display_widget.display_time(amount*60)
-
-    def update_timer_status(self):
-        self.timer.toggle()
-        if self.timer.is_ticking:
-            msg = "started"
-        else:
-            msg = "stopped"
-        self.show_message(msg)
 
     def show_message(self, msg):
         self.statusBar().showMessage(msg)
@@ -147,7 +131,9 @@ class Pomodoro(QtWidgets.QMainWindow):
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    ex = Pomodoro(25)
+    sound_fname = "/home/lefevre/phd_git/sounds/sms-alert-4-daniel_simon.wav"
+    minutes = 25
+    ex = Pomodoro(minutes, sound_fname)
     ex.show()
     sys.exit(app.exec_())
 
